f80eac6b5435a4709b2cd01f2cd4d7b480e25240
Patrick Massot <patrickmassot@free.fr>
2021-08-14 20:26:23 +0200

    Headers

--- src/topology/algebra/nonarchimedean/bases.lean
+++ src/topology/algebra/nonarchimedean/bases.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2021 Patrick Massot. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Patrick Massot
+-/
+
 import algebra.algebra.basic
 import algebra.module.submodule
 import topology.algebra.nonarchimedean.basic
